4265f8e26b67abb69e7244c2fd3c066686163c9e
이상목 <yoochang4176@naver.com>
2024-06-05 18:57:05 +0900

    Feat: "데이터 모델 구성"

--- app/src/main/java/com/sangmoki/must_go_restaurant/MainActivity.kt
+++ app/src/main/java/com/sangmoki/must_go_restaurant/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = mutableListOf<DataModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        items.add(
+            DataModel(
+
+            )
+        )
     }
 }
