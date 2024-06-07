77b10d8c76f0ff604282bd90171b239e9c1205dd
이상목 <yoochang4176@naver.com>
2024-06-07 12:10:59 +0900

    Feat: "데이터 추가 및 어댑터 연결"

--- app/src/main/java/com/sangmoki/must_go_restaurant/MainActivity.kt
+++ app/src/main/java/com/sangmoki/must_go_restaurant/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +20,21 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // 데이터 추가
         items.add(
             DataModel(
-
+                url = "https://map.naver.com/p/search/%EB%B6%88%EC%AD%88%EA%BE%B8%EB%AF%B8/place/35749600?placePath=%3Fentry%3Dpll%26from%3Dnx%26fromNxList%3Dtrue&placeSearchOption=entry%3Dpll%26fromNxList%3Dtrue&searchType=place",
+                titleImageUrl = "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230725_245%2F1690279577119GgE7p_JPEG%2F20230725_190024.jpg",
+                titleText = "불광동 불쭈꾸미"
             )
         )
+
+        // 어댑터 연결
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_restaurant)
+        val rvAdapter = RvAdapter(items)
+        recyclerView.adapter = rvAdapter
+
+        recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+
     }
 }
